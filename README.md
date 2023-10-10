5aae60fc96b6eca8fc8174be3c29f396073e842a
arkoamtiaz <147322125+arkoamtiaz@users.noreply.github.com>
2023-10-10 13:23:17 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# confession :D
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Arko-Amtiaz-the-lessful/pen/NWeovZY](https://codepen.io/Arko-Amtiaz-the-lessful/pen/NWeovZY).
+
---
+++ script.js
@@ -0,0 +1,15 @@
+function move(elm) {
+    elm.style.possiton = 'absolute'
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + '%'
+    elm.style.left = Math.floor(Math.random()* 90 + 5 ) + '%'
+}
+
+var btn = document.querySelector('#no');
+btn.addEventListener('mouseover', function(e) { 
+    move(e.target)
+})
+
+var btn = document.querySelector('#yes');
+btn.addEventListener('click', function() {
+    alert("So, we date now? 😳")
+})
